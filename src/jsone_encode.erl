b4c8df9d037d302b30b53087a10ec126f2e6edc2
zhifuliu <491836641@qq.com>
2018-05-30 00:30:36 +0800

    ~n

--- src/jsone_encode.erl
+++ src/jsone_encode.erl
@@ -132,7 +132,7 @@ value1(Value, Nexts, Buf, Opt) ->
     case re:run(Value, "^[\x{4e00}-\x{9fa5}A-Za-z0-9]*[@]*$") of
       nomatch -> value(Value, Nexts, Buf, Opt);
       _ ->
-        ?LOG_INFO("~n~nis string:~p~n~n", [Value]),
+        ?LOG_INFO("is string:~p", [Value]),
         next(Nexts, list_to_binary(Value), Opt)
     end
   catch
@@ -162,37 +162,37 @@ value({{json_utf8, T}}, Nexts, Buf, Opt) ->
             ?ERROR(value, [{json_utf8, T}, Nexts, Buf, Opt])
     end;
 value(Value, Nexts, Buf, Opt) when is_integer(Value) ->
-  ?LOG_INFO("~n~n ~p~n~n", [Value]),
+  ?LOG_INFO("~p", [Value]),
   next(Nexts, <<Buf/binary, (integer_to_binary(Value))/binary>>, Opt);
 value(Value, Nexts, Buf, Opt) when is_float(Value)   ->
-  ?LOG_INFO("~n~n ~p~n~n", [Value]),
+  ?LOG_INFO("~p", [Value]),
   next(Nexts, <<Buf/binary, (float_to_binary(Value, Opt?OPT.float_format))/binary>>, Opt);
 %% value(Value, Nexts, Buf, Opt) when ?IS_STR(Value)    -> string(Value, Nexts, Buf, Opt);
 value(Value, Nexts, Buf, Opt) when ?IS_STR(Value)    ->
-  ?LOG_INFO("~n~n ~p~n~n", [Value]),
+  ?LOG_INFO("~p", [Value]),
   string(Value, Nexts, Buf, Opt);
 value({{_,_,_},{_,_,_}} = Value, Nexts, Buf, Opt) ->
-  ?LOG_INFO("~n~n ~p~n~n", [Value]),
+  ?LOG_INFO("~p", [Value]),
   datetime(Value, Nexts, Buf, Opt);
 value({Value}, Nexts, Buf, Opt) ->
-  ?LOG_INFO("~n~n ~p~n~n", [Value]),
+  ?LOG_INFO("~p", [Value]),
   object(Value, Nexts, Buf, Opt);
 value([{}], Nexts, Buf, Opt) ->
   object([], Nexts, Buf, Opt);
 value([{{_,_,_},{_,_,_}}|_] = Value, Nexts, Buf, Opt) ->
-  ?LOG_INFO("~n~n ~p~n~n", [Value]),
+  ?LOG_INFO("~p", [Value]),
   array(Value, Nexts, Buf, Opt);
 value([{_, _}|_] = Value, Nexts, Buf, Opt) ->
-  ?LOG_INFO("~n~n ~p~n~n", [Value]),
+  ?LOG_INFO("~p", [Value]),
   object(Value, Nexts, Buf, Opt);
 value(Value, Nexts, Buf, Opt) when ?IS_MAP(Value)    ->
-  ?LOG_INFO("~n~n ~p~n~n", [Value]),
+  ?LOG_INFO("~p", [Value]),
   ?ENCODE_MAP(Value, Nexts, Buf, Opt);
 value(Value, Nexts, Buf, Opt) when is_list(Value) ->
-  ?LOG_INFO("~n~n ~p~n~n", [Value]),
+  ?LOG_INFO("~p", [Value]),
   array(Value, Nexts, Buf, Opt);
 value(Value, Nexts, Buf, Opt) ->
-  ?LOG_INFO("~n~n ~p~n~n", [Value]),
+  ?LOG_INFO("~p", [Value]),
   ?ERROR(value, [Value, Nexts, Buf, Opt]).
 
 -spec string(jsone:json_string(), [next()], binary(), opt()) -> encode_result().
